f5a9e13836f1c62dc8de483ce1f2a59f2a5520f7
David Rocok <d.bravo05@ufromail.cl>
2025-05-14 16:17:01 -0400

    Se utiliza un notebook para llamar a las funciones que se encuentran dentro del archivo .py

--- src/BankMarketing.py
+++ src/BankMarketing.py
@@ -1,28 +1,30 @@
 import os
 from dotenv import load_dotenv
 
-# Cargar variables desde .env antes de importar kaggle
-load_dotenv()
+def auth_kaggle():
+    # Cargar variables desde .env antes de importar kaggle
+    load_dotenv()
 
-# Usar la ruta del archivo JSON desde la variable de entorno
-kaggle_config_dir = os.getenv("KAGGLE_CONFIG_DIR")
+    # Usar la ruta del archivo JSON desde la variable de entorno
+    kaggle_config_dir = os.getenv("KAGGLE_CONFIG_DIR")
 
-if not kaggle_config_dir:
-    raise ValueError("KAGGLE_CONFIG_DIR no está definida en el archivo .env")
+    if not kaggle_config_dir:
+        raise ValueError("KAGGLE_CONFIG_DIR no está definida en el archivo .env")
 
-if not os.path.exists(os.path.join(kaggle_config_dir, 'kaggle.json')):
-    raise FileNotFoundError(f"No se encontró kaggle.json en la ruta: {kaggle_config_dir}")
+    if not os.path.exists(os.path.join(kaggle_config_dir, 'kaggle.json')):
+        raise FileNotFoundError(f"No se encontró kaggle.json en la ruta: {kaggle_config_dir}")
 
-# Definir la variable de entorno para que kaggle la encuentre
-os.environ['KAGGLE_CONFIG_DIR'] = kaggle_config_dir
+    # Definir la variable de entorno para que kaggle la encuentre
+    os.environ['KAGGLE_CONFIG_DIR'] = kaggle_config_dir
 
-# Importar kaggle *después* de haber definido la ruta
-import kaggle
+    # Importar kaggle *después* de haber definido la ruta
+    import kaggle
 
-# Autenticación
-kaggle.api.authenticate()
+    # Autenticación
+    kaggle.api.authenticate()
 
 
-
-#Descargar el csv
-kaggle.api.dataset_download_files("henriqueyamahata/bank-marketing", path="./data",unzip=True)
+def descargar_dataset():
+    import kaggle
+    #Descargar el csv
+    kaggle.api.dataset_download_files("henriqueyamahata/bank-marketing", path="./data",unzip=True)
